cd31d2e526309045f25fd68ec1e4667bb1a15843
Jakub Elżbieciak <jae@agnitio.com>
2013-08-08 16:00:03 +0200

    runners refactored

--- Runner/ClassGenerationRunner.php
+++ Runner/ClassGenerationRunner.php
@@ -1,10 +1,23 @@
 <?php
 
-use Xsolve\UnitSkelgenBundle\Runner\AbstractGenerationRunner;
-
 namespace Xsolve\UnitSkelgenBundle\Runner;
+
+use Xsolve\UnitSkelgenBundle\Metadata\ArgumentsMetadata;
+use Xsolve\UnitSkelgenBundle\Metadata\LocationMetadata;
 
 class ClassGenerationRunner extends AbstractGenerationRunner
 {
-    
+    protected function createArgumentsMetadata(LocationMetadata $locationMetadata)
+    {
+        $resultFilename = $this->nameTools
+            ->createProductionClassFilename($locationMetadata->getFilename());
+        $resultQualifiedClassName = $this->nameTools
+            ->createQualifiedClassName($resultFilename);
+        
+        $args = new ArgumentsMetadata('class', $locationMetadata);
+        $args
+            ->setResultFilename($resultFilename)
+            ->setResultQualifiedClassName($resultQualifiedClassName);
+        return $args;
+    }   
 }
--- Runner/TestGenerationRunner.php
+++ Runner/TestGenerationRunner.php
@@ -2,7 +2,22 @@
 
 namespace Xsolve\UnitSkelgenBundle\Runner;
 
+use Xsolve\UnitSkelgenBundle\Metadata\ArgumentsMetadata;
+use Xsolve\UnitSkelgenBundle\Metadata\LocationMetadata;
+
 class TestGenerationRunner extends AbstractGenerationRunner
-{
-    
+{    
+    protected function createArgumentsMetadata(LocationMetadata $locationMetadata)
+    {
+        $resultFilename = $this->nameTools
+            ->createTestFilename($locationMetadata->getFilename());
+        $resultQualifiedClassName = $this->nameTools
+            ->createQualifiedClassName($resultFilename);
+        
+        $args = new ArgumentsMetadata('test', $locationMetadata);
+        $args
+            ->setResultFilename($resultFilename)
+            ->setResultQualifiedClassName($resultQualifiedClassName);
+        return $args;
+    }
 }
